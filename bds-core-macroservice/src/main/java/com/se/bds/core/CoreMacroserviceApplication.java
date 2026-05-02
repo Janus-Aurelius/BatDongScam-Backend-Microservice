59edd5c8c098a062797db87d2f8d16b07c2f2c1f
Thien An Nguyen <annguyenthien.work@gmail.com>
2026-05-02 17:20:39 +0000

    refactor: migrate project to multi-module architecture and upgrade to Java 25

---
+++ bds-core-macroservice/src/main/java/com/se/bds/core/CoreMacroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.se.bds.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreMacroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreMacroserviceApplication.class, args);
+    }
+}
