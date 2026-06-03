5a92573a05e8efeb81b13f715344a98866b5726a
Thien An Nguyen <annguyenthien.work@gmail.com>
2026-06-03 15:02:18 +0000

    feat: set up microservice infrastructure with docker-compose, database initialization, and environment configurations

---
+++ init-db/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE batdongsan_db;
+CREATE DATABASE "bds-iam-service-db";
+CREATE DATABASE "bds-appointment-service-db";
+CREATE DATABASE "bds-financial-service-db";
+CREATE DATABASE "bds-moderation-service-db";
+CREATE DATABASE bds;
